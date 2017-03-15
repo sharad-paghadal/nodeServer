822f3703582fa214f3aac2e807fb559ec7bb0593
Sharad Paghadal <sharad.appdeveloper@gmail.com>
2017-03-15 15:35:18 +0530

    fully functional notification, no issue…

--- server.js
+++ server.js
@@ -8,26 +8,23 @@ server.listen(8088);
 
 console.log("server is running on 8088 port...");
 
-io.sockets.on('connection', function(socket){
+io.on('connection', function(socket){
 	console.log("Connected");
-
-	var connection;
-
-	r.connect({host: 'localhost', port: 28015}, function(err, conn) {
-	    if (err) throw err;
-	    connection = conn;
-	    r.table("fake_data").changes().run(connection, function(err, cursor) {
-	        if (err) throw err;
-	        cursor.each(function(err, item) {
-	            if (err) throw err;
-	            var data = item['new_val'];
-	            console.log("Added document", data);
-	            io.sockets.emit( 'message', { name: data['name'], message: data['message'] } );
-	        });
-		});
-	});
-
 	socket.on('disconnect', function(data){
 		console.log("Disconnected");
+	});
+});
+
+r.connect({host: 'localhost', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+    r.table("fake_data").changes().run(connection, function(err, cursor) {
+        if (err) throw err;
+        cursor.each(function(err, item) {
+            if (err) throw err;
+            var data = item['new_val'];
+            console.log("Added document", data);
+            io.sockets.emit( 'message', { name: data['name'], message: data['message'] } );
+        });
 	});
 });
