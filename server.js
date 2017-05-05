9140dd2523b2034fb1603041587390405e40b49e
Sharad Paghadal <sharad.appdeveloper@gmail.com>
2017-05-05 19:13:23 +0530

    Line chart integrated with node complete. 	Old data loaded with php api and current will be pushed by node server

--- server.js
+++ server.js
@@ -9,14 +9,30 @@ server.listen(8088);
 console.log("server is running on 8088 port...");
 
 io.on('connection', function(socket){
-	console.log("Connected");
-	socket.on('disconnect', function(data){
-		console.log("Disconnected");
-	});
-    socket.on("message", function (data) {
+    var type
+    console.log("Connected");
+
+    socket.on('disconnect', function(data){
+        console.log("Disconnected");
+    });
+
+    socket.on("request", function (data) {
         data = JSON.parse(data);
         console.log(data);
-        io.sockets.emit( 'message', { name: data['author'], message: data['message'] } );
+
+        r.connect({host: 'localhost', port: 28015}, function(err, conn) {
+            if (err) throw err;
+            connection = conn;
+            r.db("protrade").table("rawvalue").filter({code : data['code']}).pluck('current_price','time_stamp').changes().run(connection, function(err, cursor) {
+                if (err) throw err;
+                cursor.each(function(err, item) {
+                    if (err) throw err;
+                    var data = item['new_val'];
+                    console.log("Added document on console", data);
+                    socket.emit( 'upDateData', data);
+                });
+            });
+        });
     });
 });
 
